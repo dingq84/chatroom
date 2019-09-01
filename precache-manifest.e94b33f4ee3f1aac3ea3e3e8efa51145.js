d93b8ec9b7ca879b489700b194872323bf56627e
dingq84 <dingq84@gmail.com>
2019-09-01 13:29:00 +0800

    Updates

---
+++ precache-manifest.e94b33f4ee3f1aac3ea3e3e8efa51145.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f290477c31a57b50709d16b27ddc4d6",
+    "url": "/chatroom/index.html"
+  },
+  {
+    "revision": "f1b392d0af459a6588ba",
+    "url": "/chatroom/static/css/main.ccfd26dd.chunk.css"
+  },
+  {
+    "revision": "399f92b02e8bc5d94b59",
+    "url": "/chatroom/static/js/2.5425e080.chunk.js"
+  },
+  {
+    "revision": "f1b392d0af459a6588ba",
+    "url": "/chatroom/static/js/main.124c65b1.chunk.js"
+  },
+  {
+    "revision": "a2612f4e52728bca94c5",
+    "url": "/chatroom/static/js/runtime~main.1722e496.js"
+  },
+  {
+    "revision": "dcac8abdb215c7d074b6b88467a077ef",
+    "url": "/chatroom/static/media/Cog wheel silhouette free icon.dcac8abd.svg"
+  },
+  {
+    "revision": "8ce327cbacd788a87a4628c6d028e8c5",
+    "url": "/chatroom/static/media/Padlock free icon.8ce327cb.svg"
+  },
+  {
+    "revision": "f882b8d6bde4ae01ea77ca4efba522cd",
+    "url": "/chatroom/static/media/Paper clip outline free icon.f882b8d6.svg"
+  },
+  {
+    "revision": "1fcbe4304ce7c52cdd0bc244ac9f859b",
+    "url": "/chatroom/static/media/Picture free icon.1fcbe430.svg"
+  },
+  {
+    "revision": "b96c4c155ffa67878b48806724e08742",
+    "url": "/chatroom/static/media/User shape free icon.b96c4c15.svg"
+  },
+  {
+    "revision": "bebd4b5acdab8b42c78117ac69789c87",
+    "url": "/chatroom/static/media/clear.bebd4b5a.svg"
+  },
+  {
+    "revision": "b1a79cf63a01e84cd483d04aa234b7b9",
+    "url": "/chatroom/static/media/right-arrow.b1a79cf6.svg"
+  }
+]);
